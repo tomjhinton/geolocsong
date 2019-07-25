b74f799740d0b6b5aee9881814b7250f669dca8c
Tom Hinton <tomjhinton@gmail.com>
2019-07-25 13:48:35 +0100

    setup

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import 'bulma'
+import './style.scss'
+
+
+class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+
+      <Router>
+        <main>
+
+          HIYA
+
+
+        </main>
+      </Router>
+
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
